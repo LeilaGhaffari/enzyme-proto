ccca7bae153842af3a430453debd42ea418d0baf
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2021-12-06 07:10:29 -0700

    test019: Derivative of a vector wrt a scalar

---
+++ tests/test016.c
@@ -0,0 +1,81 @@
+// Derivative of a scalar with respect to a scalar
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+int  __enzyme_augmentsize(void *, ...);
+void __enzyme_augmentfwd(void *, ...);
+void __enzyme_reverse(void *, ...);
+int enzyme_dup, enzyme_allocated, enzyme_tape, enzyme_const;
+
+void square(double *x, double *y, double *a) {
+    double time = a[0];
+    y[0] = time*time*time + time*time*x[0] + time/2.;
+}
+
+void fwd_a(double *x, double *y, double *a, void *tape) {  
+    __enzyme_augmentfwd((void*)square, 
+                        enzyme_allocated, sizeof(tape[0]), 
+                        enzyme_tape, tape, 
+                        enzyme_const, x,
+                        y, (double *)NULL,
+                        a, (double *)NULL
+                       );
+}
+
+void rev_a(double *a_, double *x, void *tape) {
+    double y_ = 1.;
+    __enzyme_reverse((void*)square, 
+                     enzyme_allocated, sizeof(tape[0]), 
+                     enzyme_tape, tape, 
+                     enzyme_const, x, 
+                     (double *)NULL, &y_,
+                     (double *)NULL, a_
+                    );
+}
+
+int main() {
+    // Declarations
+    double a[] = {1, 2, 3, 4, 5};
+    double x[] = {.5, 2.5, 5., 8., 10.};
+    double y[5];
+    void *tape[5];
+
+    // Get tape size
+    int tape_size = __enzyme_augmentsize((void*)square, enzyme_const, enzyme_dup, enzyme_dup);
+
+    // Allocate memory for tape
+    for (int i=0; i<5; i++) tape[i] = malloc(tape_size);
+
+    // Forward mode
+    for (int i=0; i<5; i++) {
+        fwd_a(&x[i], &y[i], &a[i], tape[i]);
+    }
+
+    printf("a,  x,  y,  a_\n\n");
+    // Reverse mode
+    for (int i=0; i<5; i++) {
+        double a_ = 0.;
+        rev_a(&a_, &x[i], tape[i]);
+        printf("%g, %g, %g: %g\n", a[i], x[i], y[i], a_);
+    }
+
+    // Free tape
+    for (int i=0; i<5; i++) free(tape[i]);
+
+    return 0;
+}
+
+/*
+clang test016.c -Xclang -load -Xclang /home/leila/Enzyme/enzyme/build12DHB/Enzyme/ClangEnzyme-12.so -O2 -fno-vectorize -fno-unroll-loops
+
+a,  x,  y,  a_
+
+1, 0.5, 2: 4.5
+2, 2.5, 19: 22.5
+3, 5, 73.5: 57.5
+4, 8, 194: 112.5
+5, 10, 377.5: 175.5
+
+*/
---
+++ tests/test017.c
@@ -0,0 +1,104 @@
+// Mock Qdot in libCEED (derivative of a vector wrt a scalar)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+int  __enzyme_augmentsize(void *, ...);
+void __enzyme_augmentfwd(void *, ...);
+void __enzyme_reverse(void *, ...);
+int enzyme_dup, enzyme_allocated, enzyme_tape, enzyme_const, enzyme_nofree;
+
+void square(double *x, double *y, double *A) {
+    double a = A[0];
+    y[0] = a*a*a + a*a*x[0] + a/2.;
+    y[1] = a*a*a + a*a*x[1] + a/2.;
+    y[2] = a*a*a + a*a*x[2] + a/2.;
+    y[3] = a*a*a + a*a*x[3] + a/2.;
+    y[4] = a*a*a + a*a*x[4] + a/2.;
+}
+
+int getTapeSize() {
+  return __enzyme_augmentsize((void *)square, enzyme_const, enzyme_dup, enzyme_dup);
+}
+
+void fwd_a(double *x, double *y, double *a, void *tape) {  
+    __enzyme_augmentfwd((void*)square, 
+                        enzyme_allocated, sizeof(tape[0]), 
+                        enzyme_tape, tape, 
+                        enzyme_const, x,
+                        y, (double *)NULL,
+                        a, (double *)NULL
+                       );
+}
+
+void rev_a(double *x, double *y_, double *a_, void *tape, bool free) {
+    if (free)
+      __enzyme_reverse((void*)square, 
+                       enzyme_allocated, sizeof(tape[0]), 
+                       enzyme_tape, tape, 
+                       enzyme_const, x, 
+                       (double *)NULL, y_,
+                       (double *)NULL, a_
+                      );
+    else
+      __enzyme_reverse((void*)square, 
+                       enzyme_allocated, sizeof(tape[0]), 
+                       enzyme_tape, tape, enzyme_nofree, 
+                       enzyme_const, x, 
+                       (double *)NULL, y_,
+                       (double *)NULL, a_
+                      );                      
+}
+
+int main() {
+    // Declarations
+    double x[] = {.5, 2.5, 5., 8., 10.};
+    double y[5];
+    void *tape;
+
+    // Get tape size
+    int tape_size = getTapeSize();
+
+    // Allocate memory for tape
+    tape = malloc(tape_size);
+
+    // Forward mode
+    double a[1] = {5.};
+    fwd_a(x, y, a, tape);
+
+    // Reverse mode
+    double a_[5] = {0.};
+    for (int i=0; i<5; i++) {
+        double y_[5] = {0.}; y_[i] = 1.;
+        rev_a(x, y_, &a_[i], tape, false);
+    }
+    
+    // Print output
+    printf("\na,  x,  y,  a_\n");
+    for (int i=0; i<5; i++) printf("%g, %g, %g: %g\n", a[i], x[i], y[i], a_[i]);
+    
+    // Free tape
+    {
+        double y_[5] = {0.}; y_[0] = 1.;
+        rev_a(x, y_, a_, tape, true);
+        free(tape);
+    }
+
+    return 0;
+}
+
+/*
+clang test017.c -Xclang -load -Xclang /home/leila/Enzyme/enzyme/build12DHB/Enzyme/ClangEnzyme-12.so -O2 -fno-vectorize -fno-unroll-loops
+
+Output:
+
+a  x    y    a_
+5, 0.5, 140: 80.5
+5, 2.5, 190: 100.5
+5, 5, 252.5: 125.5
+5, 8, 327.5: 155.5
+5, 10, 377.5: 175.5
+
+*/
