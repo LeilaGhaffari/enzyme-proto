0c333b937676ae8aa3871fd6ef378e3269411f51
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2021-12-15 10:05:51 -0700

    test025: pass spatial variables as a vector

--- tests/test025.c
+++ tests/test025.c
@@ -1,4 +1,4 @@
-// Compute d2q/dx2
+// Compute d2q/dx2 - x[3] passed in
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,13 +8,13 @@
 void __enzyme_autodiff(void *, ...);
 int enzyme_const;
 
-int ExactSolution(double q[], double *time, double *X, double *Y, double *Z) {
+int ExactSolution(double q[], double *time, double X[]) {
   // -- Time
   double t = time[0];
   // -- Coordinates
   double x = X[0];
-  double y = Y[0];
-  double z = Z[0];
+  double y = X[1];
+  double z = X[2];
 
   // Exact solutions
   q[0] = 1.*x*x+x + 6. *y*y+y + 11.*z*z+z;
@@ -27,57 +27,33 @@ int ExactSolution(double q[], double *time, double *X, double *Y, double *Z) {
 }
 
 // -- grad_q
-void computeGrad_q(double grad_q[3][5], double *t, double *x, double *y, double *z) {
+void computeGrad_q(double grad_q[5][3], double *t, double x[]) {
   double q[5];
-  // Derivative wrt x
   for (int i=0; i<5; i++) {
-    double q_[5] = {0.}; q_[i] = 1.;
+    double q_[5] = {0.}; q_[i]= 1.;
     __enzyme_autodiff((void *)ExactSolution,
                       q, q_,
                       enzyme_const, t,
-                      x, &grad_q[0][i],
-                      enzyme_const, y,
-                      enzyme_const, z);
-  }
-  // Derivative wrt y
-  for (int i=0; i<5; i++) {
-    double q_[5] = {0.}; q_[i] = 1.;
-    __enzyme_autodiff((void *)ExactSolution,
-                      q, q_,
-                      enzyme_const, t,
-                      enzyme_const, x,
-                      y, &grad_q[1][i],
-                      enzyme_const, z);
-  }
-  // Derivative wrt z
-  for (int i=0; i<5; i++) {
-    double q_[5] = {0.}; q_[i] = 1.;
-    __enzyme_autodiff((void *)ExactSolution,
-                      q, q_,
-                      enzyme_const, t,
-                      enzyme_const, x,
-                      enzyme_const, y,
-                      z, &grad_q[2][i]);
+                      x, grad_q[i]);          
   }
 }
 
-void compute_dq_x(double dq[5], double *t, double *x, double *y, double *z) {
-  double grad_q[3][5];
-  computeGrad_q(grad_q, t, x, y, z);
-  for (int i=0; i<5; i++) dq[i] = grad_q[0][i];
+void compute_dq_x(double dq[5], double *t, double x[]) {
+  double grad_q[5][3];
+  computeGrad_q(grad_q, t, x);
+  for (int i=0; i<5; i++) dq[i] = grad_q[i][0];
 }
 
-void compute_d2q_x(double dq[5], double *t, double *x, double *y, double *z) {
+void compute_d2q_x(double dq[5], double *t, double x[]) {
   double dq_x[5];
-  // Derivative wrt x
   for (int i=0; i<5; i++) {
-    double q_[5] = {0.}; q_[i] = 1.;
+    double q_[5] = {0.}; q_[i]= 1.;
+    double dq_[3] = {0.};
     __enzyme_autodiff((void *)compute_dq_x,
                       dq_x, q_,
                       enzyme_const, t,
-                      x, &dq[i],
-                      enzyme_const, y,
-                      enzyme_const, z);
+                      x, dq_);
+    dq[i] = dq_[0];
   }
 }
 
@@ -87,23 +63,23 @@ void compute_d2q_x(double dq[5], double *t, double *x, double *y, double *z) {
 int main() {
   // Declarations
   double X[] = {.5, 2.5, 5.};
-  double x[1] = {X[0]}, y[1] = {X[1]}, z[1] = {X[2]};
+  //double x[1] = {X[0]}, y[1] = {X[1]}, z[1] = {X[2]};
   double time[1] = {.2};
-  // -------------------------------------------------------------------------
+// -------------------------------------------------------------------------
   // grad_q
   // -------------------------------------------------------------------------
-  double grad_q[3][5] = {{0.}};
-  computeGrad_q(grad_q, time, x, y, z);
+  double grad_q[5][3] = {{0.}};
+  computeGrad_q(grad_q, time, X);
   for (int i=0; i<3; i++) {
     printf("\nDerivative in direction %d:\n", i);
-    for (int j=0; j<5; j++) printf("%f\t", grad_q[i][j]);
+    for (int j=0; j<5; j++) printf("%f\t", grad_q[j][i]);
     printf("\n");
   }
   // -------------------------------------------------------------------------
   // d2q/dx2
   // -------------------------------------------------------------------------
   double d2q_dx2[5] = {0.}; // Must initialize
-  compute_d2q_x(d2q_dx2, time, x, y, z);
+  compute_d2q_x(d2q_dx2, time, X);
   // Print output
   printf("\nd2q/dx2:\n");
   for (int j=0; j<5; j++) printf("%f\t", d2q_dx2[j]);
@@ -113,7 +89,7 @@ int main() {
 }
 // ***************************************************************************
 /*
-clang test024.c -Xclang -load -Xclang /home/leila/Enzyme/enzyme/build12DHB/Enzyme/ClangEnzyme-12.so -O2 -fno-vectorize -fno-unroll-loops
+clang test025.c -Xclang -load -Xclang /home/leila/Enzyme/enzyme/build12DHB/Enzyme/ClangEnzyme-12.so -O2 -fno-vectorize -fno-unroll-loops
 
 Derivative in direction 0:
 2.000000	3.000000	4.000000	5.000000	6.000000	
