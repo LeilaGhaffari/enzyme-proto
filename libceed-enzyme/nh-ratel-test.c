d9132f48828556d8c7f94510db6e98fa8f8a7b1f
Kellen Martin <kema0161@colorado.edu>
2022-06-30 12:34:55 -0600

    WIP changes how Psi is calculated

--- libceed-enzyme/nh-ratel-test.c
+++ libceed-enzyme/nh-ratel-test.c
@@ -14,6 +14,44 @@ int enzyme_const;
 
 double StrainEnergy(double E_Voigt[VECSIZE], double mu, double lambda);
 
+ void RatelVoigtUnpack(const double sym[6], double full[3][3]) {
+  full[0][0] = sym[0];
+  full[0][1] = sym[5];
+  full[0][2] = sym[4];
+  full[1][0] = sym[5];
+  full[1][1] = sym[1];
+  full[1][2] = sym[3];
+  full[2][0] = sym[4];
+  full[2][1] = sym[3];
+  full[2][2] = sym[2];
+}
+
+int RatelMatComputeInverseSymmetric(const double A[3][3], const double det_A, double A_inv[6]) {
+  // Compute A^(-1) : A-Inverse
+  double B[6] = {
+      A[1][1] * A[2][2] - A[1][2] * A[2][1], /* *NOPAD* */
+      A[0][0] * A[2][2] - A[0][2] * A[2][0], /* *NOPAD* */
+      A[0][0] * A[1][1] - A[0][1] * A[1][0], /* *NOPAD* */
+      A[0][2] * A[1][0] - A[0][0] * A[1][2], /* *NOPAD* */
+      A[0][1] * A[1][2] - A[0][2] * A[1][1], /* *NOPAD* */
+      A[0][2] * A[2][1] - A[0][1] * A[2][2]  /* *NOPAD* */
+  };
+  for (int m = 0; m < 6; m++) {
+    A_inv[m] = B[m] / (det_A);
+  }
+
+  return 0;
+}
+
+double RatelMatDetAM1(const double A[3][3]) {
+  return A[0][0] * (A[1][1] * A[2][2] - A[1][2] * A[2][1]) +         /* *NOPAD* */
+         A[0][1] * (A[1][2] * A[2][0] - A[1][0] * A[2][2]) +         /* *NOPAD* */
+         A[0][2] * (A[1][0] * A[2][1] - A[2][0] * A[1][1]) +         /* *NOPAD* */
+         A[0][0] + A[1][1] + A[2][2] +                               /* *NOPAD* */
+         A[0][0] * A[1][1] + A[0][0] * A[2][2] + A[1][1] * A[2][2] - /* *NOPAD* */
+         A[0][1] * A[1][0] - A[0][2] * A[2][0] - A[1][2] * A[2][1];  /* *NOPAD* */
+}
+
 double log1p_series_shifted(double x) {
   double sum = 0;
   double y = x / (2. + x);
@@ -26,6 +64,7 @@ double log1p_series_shifted(double x) {
   return 2 * sum;
 };
 
+/* non-ratel functions
 double computeDetCM1(double *E2_Voigt) {
   return E2_Voigt[0]*(E2_Voigt[1]*E2_Voigt[2]-E2_Voigt[3]*E2_Voigt[3]) +
          E2_Voigt[5]*(E2_Voigt[4]*E2_Voigt[3]-E2_Voigt[5]*E2_Voigt[2]) +
@@ -47,16 +86,14 @@ int computeMatinvSym(double A[3][3], double detA, double *Ainv) {
                 };               
   for ( int m = 0; m < VECSIZE; m++) Ainv[m] = B[m] / (detA);
   return 0;
-};
+};*/
 
 void S_analytical(double S_an[VECSIZE], double E_Voigt[VECSIZE], double mu, double lambda) {
   double E2_Voigt[VECSIZE];
   for(int i = 0; i<6; i++) E2_Voigt[i] = 2*E_Voigt[i];
 
-  double E2[3][3] = {{E2_Voigt[0], E2_Voigt[5], E2_Voigt[4]},
-                     {E2_Voigt[5], E2_Voigt[1], E2_Voigt[3]},
-                     {E2_Voigt[4], E2_Voigt[3], E2_Voigt[2]}
-                    };
+  double E2[3][3];
+  RatelVoigtUnpack(E2_Voigt, E2);
    
   // C : right Cauchy-Green tensor
   double C[3][3] = {{1 + E2[0][0], E2[0][1], E2[0][2]},
@@ -65,20 +102,19 @@ void S_analytical(double S_an[VECSIZE], double E_Voigt[VECSIZE], double mu, doub
                    };
 
   // Compute C^(-1) : C-Inverse
-  double Cinvwork[VECSIZE];
-  double detCm1 = computeDetCM1(E2_Voigt);
-  computeMatinvSym(C, detCm1+1, Cinvwork);
+  double Cinv_Voigt[VECSIZE];
+  double C_inv[3][3];
+  double detCm1 = RatelMatDetAM1(E2);
+  RatelMatComputeInverseSymmetric(C, detCm1+1, Cinv_Voigt);
+  RatelVoigtUnpack(Cinv_Voigt, C_inv);
 
-  double C_inv[3][3] = {{Cinvwork[0], Cinvwork[5], Cinvwork[4]},
-                        {Cinvwork[5], Cinvwork[1], Cinvwork[3]},
-                        {Cinvwork[4], Cinvwork[3], Cinvwork[2]}
-                       };
+
 
   // Compute the Second Piola-Kirchhoff (S)
   int indj[VECSIZE] = {0, 1, 2, 1, 0, 0}, indk[VECSIZE] = {0, 1, 2, 2, 2, 1};
   double logJ = log1p_series_shifted(detCm1) / 2.;
   for ( int m = 0; m < VECSIZE; m++) {
-      S_an[m] = lambda*logJ*Cinvwork[m];;
+      S_an[m] = lambda*logJ*Cinv_Voigt[m];;
       for ( int n = 0; n < 3; n++)
           S_an[m] += mu*C_inv[indj[m]][n]*E2[n][indk[m]];
   }
@@ -90,13 +126,17 @@ double StrainEnergy(double E_Voigt[VECSIZE], double mu, double lambda) {
   for(int i = 0; i<6; i++) E2_Voigt[i] = E_Voigt[i]*2; 
 
   // log(J)
-  double detCm1 = computeDetCM1(E2_Voigt);
+  double E2[3][3];
+  RatelVoigtUnpack(E2_Voigt, E2);
+  double detCm1 = RatelMatDetAM1(E2);
+  double J = sqrt(detCm1 + 1);
   double logJ = log1p_series_shifted(detCm1) / 2.;
 
   // trace(E)
   double traceE = (E_Voigt[0] + E_Voigt[1] + E_Voigt[2]);
 
-  return lambda*logJ*logJ/2. + mu * (-logJ + traceE);
+  return lambda*(J*J - 1)/4 - lambda*logJ/2  + mu * (-logJ + traceE);
+  // return lambda*logJ*logJ/2  + mu * (-logJ + traceE);
 };
 
 void S_autodiff(double *S_ad, double *E_Voigt, double mu, double lambda) {
