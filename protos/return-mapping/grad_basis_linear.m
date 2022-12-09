78a23e4db4c2534bae69220324c0e67571744741
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2022-12-09 13:19:32 -0700

    Clean up coaxial values and some more

--- protos/return-mapping/grad_basis_linear.m
+++ protos/return-mapping/grad_basis_linear.m
@@ -1,29 +1,30 @@
-function dNdX = grad_basis_linear(xi, eta, zeta)
+function dNdX = grad_basis_linear(xi)
+
     % derivatives of shape functions with respect to xi
-    dN1_dxi = -0.125*(1-eta)*(1-zeta);
+    dN1_dxi = -0.125*(1-xi(2))*(1-xi(3));
     dN2_dxi = -dN1_dxi;
-    dN3_dxi = 0.125*(1+eta)*(1-zeta);
+    dN3_dxi = 0.125*(1+xi(2))*(1-xi(3));
     dN4_dxi = -dN3_dxi;
-    dN5_dxi = -0.125*(1-eta)*(1+zeta);
+    dN5_dxi = -0.125*(1-xi(2))*(1+xi(3));
     dN6_dxi = -dN5_dxi;
-    dN7_dxi = 0.125*(1+eta)*(1+zeta);
+    dN7_dxi = 0.125*(1+xi(2))*(1+xi(3));
     dN8_dxi = -dN7_dxi;
 
     % derivatives of shape functions with respect to eta
-    dN1_deta = -0.125*(1-xi)*(1-zeta);
-    dN2_deta = -0.125*(1+xi)*(1-zeta);
+    dN1_deta = -0.125*(1-xi(1))*(1-xi(3));
+    dN2_deta = -0.125*(1+xi(1))*(1-xi(3));
     dN3_deta = -dN2_deta;
     dN4_deta = -dN1_deta;
-    dN5_deta = -0.125*(1-xi)*(1+zeta);
-    dN6_deta = -0.125*(1+xi)*(1+zeta);
+    dN5_deta = -0.125*(1-xi(1))*(1+xi(3));
+    dN6_deta = -0.125*(1+xi(1))*(1+xi(3));
     dN7_deta = -dN6_deta;
     dN8_deta = -dN5_deta;
 
     % derivatives of shape functions with respect to zeta
-    dN1_dzeta = -0.125*(1-xi)*(1-eta);
-    dN2_dzeta = -0.125*(1+xi)*(1-eta);
-    dN3_dzeta = -0.125*(1+xi)*(1+eta);
-    dN4_dzeta = -0.125*(1-xi)*(1+eta);
+    dN1_dzeta = -0.125*(1-xi(1))*(1-xi(2));
+    dN2_dzeta = -0.125*(1+xi(1))*(1-xi(2));
+    dN3_dzeta = -0.125*(1+xi(1))*(1+xi(2));
+    dN4_dzeta = -0.125*(1-xi(1))*(1+xi(2));
     dN5_dzeta = -dN1_dzeta;
     dN6_dzeta = -dN2_dzeta;
     dN7_dzeta = -dN3_dzeta;
--- protos/return-mapping/return_map_exponential.m
+++ protos/return-mapping/return_map_exponential.m
@@ -2,7 +2,6 @@
 % Test with:
 %   meld q_stress_src.txt q_stress.log; meld q_Fp_src.txt q_Fp.log ; meld q_isv_src.txt q_isv.log
 
-clc
 clear all
 
 coordsx = [ 0.0  0.0  0.0;
@@ -55,7 +54,10 @@ save     q_Fp.log     Fp_q -ASCII
 % -------------------------------------------------------------------------------------------------
 function [stress_el,isv_el,Fp_el] = element_stress_isv(coordsx,d,params,c_tau_n,Fp_el_n,theta)
 
-    %params = [ lambda mu Aphi Bphi Apsi Bpsi Hc numips nstress nisv ndim ];
+    % Helper function: 3x3 identity matrix
+    eye_mat = eye(3);
+
+    % params = [ lambda mu Aphi Bphi Apsi Bpsi Hc numips nstress nisv ndim ];
     lambda  = params(1);
     mu      = params(2);
     Aphi    = params(3);
@@ -67,30 +69,22 @@ function [stress_el,isv_el,Fp_el] = element_stress_isv(coordsx,d,params,c_tau_n,
     nisv    = round(params(10));
     ndim    = round(params(11));
 
-    % tolerances for local Newton-Raphson algorithm to solve for Delta gamma
-    fyield_atol = 1e-6;
-    fyield_rtol = 1e-8;
-
-
-    % initialize size of stress, and ISVs at Gauss pts, to be returned
+    % initialize
     isv_el    = zeros(nisv);
     Fp_el     = zeros(ndim, ndim);
     stress_el = zeros(ndim, ndim, nstress);
 
     % integration points in natural coordinates (this is for the fifth quadrature point)
-    xi   = -1 / sqrt(3);
-    eta  = -1 / sqrt(3);
-    zeta =  1 / sqrt(3);
+    X = [-1 -1  1] / sqrt(3);
 
-    % gradient of shape functions
-    dNdX = grad_basis_linear(xi, eta, zeta);
+    % gradient of shape functions with respect to the natural coordinates
+    dNdX = grad_basis_linear(X);
 
-    % calculate jacobian, its determinant, and its inverse
-    Je    = dNdX * coordsx;
-    Jeinv = inv(Je);
+    % calculate jacobian
+    Je = dNdX * coordsx;
 
-    %shape function derivatives with respect to X,Y,Z
-    dN_dx = dNdX' * Jeinv;
+    % gradient of shape functions with respect to the physical coordinates
+    dN_dx = dNdX' / Je;
 
     % strain-displacement matrix
     Bu = zeros(9, 24);
@@ -99,79 +93,62 @@ function [stress_el,isv_el,Fp_el] = element_stress_isv(coordsx,d,params,c_tau_n,
     end
 
     % total deformation tensors
-    dudX     = Bu*d';
-    Fdef     = eye(3) + [ dudX(1) dudX(2) dudX(3); dudX(4) dudX(5) dudX(6); dudX(7) dudX(8) dudX(9) ];
-    Jdef     = det(Fdef);
-    Fdef_inv = inv(Fdef);
-    Cdef     = Fdef' * Fdef;
-    Cdef_inv = inv(Cdef);
-    bdef     = Fdef * Fdef';
-    bdef_inv = inv(bdef);
-    vdef     = sqrtm(bdef);
-
-    % 3x3 identity matrix
-    eye_mat = eye(3);
+    dudX = Bu * d';
+    Fdef = eye(3) + reshape(dudX, [3, 3])';
+    Jdef = det(Fdef);
+    Cdef = Fdef' * Fdef;
+    bdef = Fdef  * Fdef';
+    vdef = sqrtm(bdef);
 
     % total strains
-    Estrain  = (Cdef-eye_mat)/2;
-    estrain  = (eye_mat-inv(bdef))/2;
+    Estrain  = (Cdef - eye_mat) / 2;
+    estrain  = (eye_mat - inv(bdef)) / 2;
     eHstrain = logm(vdef);
 
     % total principal Hencky strain
     lambda_eig     = eig(eHstrain);
     eHstrain_princ = [ max(lambda_eig)  0              0 ;
-                           0                lambda_eig(2)  0 ;
-                           0                0              min(lambda_eig) ];
+                       0                lambda_eig(2)  0 ;
+                       0                0              min(lambda_eig) ];
 
     % calculate trial elastic deformation gradient and left elastic Cauchy Green tensor
-    Fp_n      = Fp_el_n(:, :);
-    Fp_n_inv  = inv(Fp_n);
-    Fe_tr     = Fdef * Fp_n_inv;
-    be_tr     = Fe_tr * Fe_tr';
-    Jdef_e_tr = det(Fe_tr);
+    Fp_n  = Fp_el_n(:, :);
+    Fe_tr = Fdef / Fp_n;
+    be_tr = Fe_tr * Fe_tr';
 
     % find eigenvalues and eigenvectors of be_tr
-    [V,D]       = eig(be_tr);
-    lambda1e_tr = sqrt(D(1, 1));
-    lambda2e_tr = sqrt(D(2, 2));
-    lambda3e_tr = sqrt(D(3, 3));
-    Jdef_e_tr   = lambda1e_tr * lambda2e_tr * lambda3e_tr;
-    n1_tr       = V(:, 1);
-    n2_tr       = V(:, 2);
-    n3_tr       = V(:, 3);
+    [V, D]      = eig(be_tr);
+    lambda_e_tr = sqrt(diag(D));
+    Jdef_e_tr   = prod(lambda_e_tr);
 
     % calculate trial Kirchhoff stress
-    tau1_tr    = lambda * log(Jdef_e_tr) - mu + mu * lambda1e_tr * lambda1e_tr;
-    tau2_tr    = lambda * log(Jdef_e_tr) - mu + mu * lambda2e_tr * lambda2e_tr;
-    tau3_tr    = lambda * log(Jdef_e_tr) - mu + mu * lambda3e_tr * lambda3e_tr;
-    tau_tr     = tau1_tr*(n1_tr*n1_tr') + tau2_tr*(n2_tr*n2_tr') + tau3_tr*(n3_tr*n3_tr');
-    p_tau_tr   = (tau1_tr + tau2_tr + tau3_tr) / 3;
-    devtau1_tr = tau1_tr - p_tau_tr;
-    devtau2_tr = tau2_tr - p_tau_tr;
-    devtau3_tr = tau3_tr - p_tau_tr;
+    tau_princ_tr   = zeros(3, 1);
+    tau_princ_coef = @(J, eigval) lambda * log(J) - mu + mu * eigval * eigval;
+    for i=1:3
+        tau_princ_tr(i) = tau_princ_coef(Jdef_e_tr, lambda_e_tr(i));
+    end
+    tau_tr           = coaxial(be_tr, tau_princ_tr);
+    p_tau_tr         = sum(tau_princ_tr) / 3;
+    dev_tau_princ_tr = tau_princ_tr - p_tau_tr;
 
     % calculate trial elastic left stretch and rotation
     ve_tr = sqrtm(be_tr);
     Re_tr = inv(ve_tr) * Fe_tr;
 
     % calculate trial yield function
-    c_tau          = c_tau_n;
-    devtau_tr_norm = sqrt(devtau1_tr*devtau1_tr + devtau2_tr*devtau2_tr + devtau3_tr*devtau3_tr);
-    f_tr           = devtau_tr_norm - Aphi*c_tau_n + Bphi*p_tau_tr;
+    c_tau           = c_tau_n;
+    dev_tau_tr_norm = norm(dev_tau_princ_tr);
+    f_tr            = dev_tau_tr_norm - Aphi * c_tau_n + Bphi * p_tau_tr;
 
-    %dgdtau_tr
-    dgdtau1_tr      = (devtau1_tr/devtau_tr_norm) + Bpsi/3;
-    dgdtau2_tr      = (devtau2_tr/devtau_tr_norm) + Bpsi/3;
-    dgdtau3_tr      = (devtau3_tr/devtau_tr_norm) + Bpsi/3;
-    trace_dgdtau_tr = dgdtau1_tr + dgdtau2_tr + dgdtau3_tr;
-    dgdtau_tr       = dgdtau1_tr*(n1_tr*n1_tr') + dgdtau2_tr*(n2_tr*n2_tr') + dgdtau3_tr*(n3_tr*n3_tr');
+    % dgdtau_tr
+    dgdtau_princ_tr = dev_tau_princ_tr / dev_tau_tr_norm + Bpsi / 3;
+    trace_dgdtau_tr = sum(dgdtau_princ_tr);
+    dgdtau_tr       = coaxial(be_tr, dgdtau_princ_tr);
 
-    %dfdtau_tr
-    dfdtau1_tr      = (devtau1_tr/devtau_tr_norm) + Bphi/3;
-    dfdtau2_tr      = (devtau2_tr/devtau_tr_norm) + Bphi/3;
-    dfdtau3_tr      = (devtau3_tr/devtau_tr_norm) + Bphi/3;
-    trace_dfdtau_tr = dfdtau1_tr + dfdtau2_tr + dfdtau3_tr;
-    dfdtau_tr       = dfdtau1_tr*(n1_tr*n1_tr') + dfdtau2_tr*(n2_tr*n2_tr') + dfdtau3_tr*(n3_tr*n3_tr');
+    % dfdtau_tr
+    dfdtau_princ_tr = dev_tau_princ_tr / dev_tau_tr_norm + Bphi / 3;
+    trace_dfdtau_tr = sum(dfdtau_princ_tr);
+    dfdtau_tr       = coaxial(be_tr, dfdtau_princ_tr);;
 
     % check if elastic, or elasto-plastic
     if (f_tr < 0)  % elastic
@@ -181,22 +158,21 @@ function [stress_el,isv_el,Fp_el] = element_stress_isv(coordsx,d,params,c_tau_n,
         c_tau = c_tau_n;
         be    = be_tr;
     else % plastic
-        lambda1e = lambda1e_tr;
-        lambda2e = lambda2e_tr;
-        lambda3e = lambda3e_tr;
-        Jdef_e   = lambda1e*lambda2e*lambda3e;
+        lambda_e = lambda_e_tr;
+        Jdef_e   = prod(lambda_e);
 
         % calculate Kirchhoff stress and deviatoric part
-        tau1        = lambda*log(Jdef_e) - mu + mu*lambda1e*lambda1e;
-        tau2        = lambda*log(Jdef_e) - mu + mu*lambda2e*lambda2e;
-        tau3        = lambda*log(Jdef_e) - mu + mu*lambda3e*lambda3e;
-        p_tau       = (tau1 + tau2 + tau3)/3;
-        devtau1     = tau1 - p_tau;
-        devtau2     = tau2 - p_tau;
-        devtau3     = tau3 - p_tau;
-        devtau_norm = sqrt(devtau1*devtau1 + devtau2*devtau2 + devtau3*devtau3);
+        tau_princ = zeros(3, 1);
+        for i=1:3
+            tau_princ(i) = tau_princ_coef(Jdef_e, lambda_e(i));
+        end
+        p_tau         = sum(tau_princ) / 3;
+        dev_tau_princ = tau_princ - p_tau;
+        dev_tau_norm  = norm(dev_tau_princ);
 
-        %solve for Dg (Delta_gamma)
+        % solve for Dg (Delta_gamma)
+        fyield_atol      = 1e-6;
+        fyield_rtol      = 1e-8;
         k                = 0;
         iter_break_local = 10;
         fyield           = f_tr;
@@ -205,19 +181,18 @@ function [stress_el,isv_el,Fp_el] = element_stress_isv(coordsx,d,params,c_tau_n,
             k = k+1;
 
             % derivative of devtau wrt Dg
-            dtau1dDg      = -lambda*trace_dgdtau_tr - 2*mu*lambda1e*lambda1e*dgdtau1_tr;
-            dtau2dDg      = -lambda*trace_dgdtau_tr - 2*mu*lambda2e*lambda2e*dgdtau2_tr;
-            dtau3dDg      = -lambda*trace_dgdtau_tr - 2*mu*lambda3e*lambda3e*dgdtau3_tr;
-            trace_dtaudDg = dtau1dDg + dtau2dDg + dtau3dDg;
-            devdtau1dDg   = dtau1dDg - trace_dtaudDg/3;
-            devdtau2dDg   = dtau2dDg - trace_dtaudDg/3;
-            devdtau3dDg   = dtau3dDg - trace_dtaudDg/3;
+            dtau_princ_dDg = zeros(3, 1);
+            for i=1:3
+                dtau_princ_dDg(i) = -lambda * trace_dgdtau_tr - 2 * mu * lambda_e(i) * lambda_e(i) * dgdtau_princ_tr(i);
+            end
+            trace_dtaudDg      = sum(dtau_princ_dDg);
+            dev_dtau_princ_dDg = dtau_princ_dDg - trace_dtaudDg / 3;
 
             % derivative of devtau_norm wrt Dg
-            ddevtau_norm_dDg = (devtau1*devdtau1dDg + devtau2*devdtau2dDg + devtau3*devdtau3dDg) / devtau_norm;
+            ddevtau_norm_dDg = dot(dev_tau_princ, dev_dtau_princ_dDg) / dev_tau_norm;
 
             % yield tangent
-            dfdDg = ddevtau_norm_dDg - Hc*Aphi*Apsi + (Bphi/3)*trace_dtaudDg;
+            dfdDg = ddevtau_norm_dDg - Hc * Aphi * Apsi + Bphi * trace_dtaudDg / 3;
 
             % increment of Dg
             del_Dg  = -fyield / dfdDg;
@@ -226,52 +201,50 @@ function [stress_el,isv_el,Fp_el] = element_stress_isv(coordsx,d,params,c_tau_n,
                 Dg_iter
             end
 
-            %update elastic stretches
-            lambda1e = lambda1e_tr * exp(-Dg_iter*dgdtau1_tr);
-            lambda2e = lambda2e_tr * exp(-Dg_iter*dgdtau2_tr);
-            lambda3e = lambda3e_tr * exp(-Dg_iter*dgdtau3_tr);
-            Jdef_e   = lambda1e*lambda2e*lambda3e;
+            % update elastic stretches
+            for i=1:3
+                lambda_e(i) = lambda_e_tr(i) * exp(-Dg_iter * dgdtau_princ_tr(i));
+            end
+            Jdef_e = prod(lambda_e);
 
             % calculate Kirchhoff stress and deviatoric part
-            tau1        = lambda*log(Jdef_e) - mu + mu*lambda1e*lambda1e;
-            tau2        = lambda*log(Jdef_e) - mu + mu*lambda2e*lambda2e;
-            tau3        = lambda*log(Jdef_e) - mu + mu*lambda3e*lambda3e;
-            p_tau       = (tau1 + tau2 + tau3)/3;
-            devtau1     = tau1 - p_tau;
-            devtau2     = tau2 - p_tau;
-            devtau3     = tau3 - p_tau;
-            devtau_norm = sqrt(devtau1*devtau1 + devtau2*devtau2 + devtau3*devtau3);
+            for i=1:3
+                tau_princ(i) = tau_princ_coef(Jdef_e, lambda_e(i));
+            end
+            p_tau        = sum(tau_princ) / 3;
+            dev_tau      = tau_princ - p_tau;
+            dev_tau_norm = norm(dev_tau);
 
             % update c_tau
-            c_tau = c_tau_n + Dg_iter*Hc*Apsi;
+            c_tau = c_tau_n + Dg_iter * Hc * Apsi;
 
             % update fyield
-            fyield = devtau_norm - Aphi*c_tau + Bphi*p_tau;
+            fyield = dev_tau_norm - Aphi * c_tau + Bphi * p_tau;
 
-            if k==iter_break_local
+            if k == iter_break_local
                 k
                 error('reached max number of local iterations for Dg')
             end
         end
-        %update Fp
+
+        % update Fp
         Fp = Re_tr' * expm(Dg_iter * dgdtau_tr) * Re_tr * Fp_n;
 
-        %update tau
-        tau = tau1*(n1_tr*n1_tr') + tau2*(n2_tr*n2_tr') + tau3*(n3_tr*n3_tr');
+        % update tau
+        tau = coaxial(be_tr, tau_princ);
 
-        %update Dg
+        % update Dg
         Dg = Dg_iter;
 
-        %update be
-        Fe_ = Fdef*inv(Fp);
-        be  = Fe_ * Fe_';
-        %be = (lambda1e^2)*(n1_tr*n1_tr') + (lambda2e^2)*(n2_tr*n2_tr') + (lambda3e^2)*(n3_tr*n3_tr'); % this is correct too
+        % update be
+        Fe = Fdef * inv(Fp);
+        be = Fe * Fe';
     end
 
     % stresses
-    Pstress = tau*Fdef_inv';
-    Sstress = Fdef_inv*Pstress;
-    sigma   = (1/Jdef)*tau;
+    sigma   = tau / Jdef;            % Cauchy stress => \sigma = J^{-1} \tau
+    Pstress = tau / Fdef';           % First Piola   => P = \tau F^{-T}
+    Sstress = inv(Fdef) * Pstress;   % Second Piola  => S = F^{-1} P
 
     % rotate stress for non-rotating case
     Qrot = [  cos(theta) sin(theta) 0;
@@ -286,28 +259,36 @@ function [stress_el,isv_el,Fp_el] = element_stress_isv(coordsx,d,params,c_tau_n,
                     0                0              min(lambda_eig) ];
 
     % order of storage: S,P,sig,E,e,le,tau,be,dfdtau,dgdtau,sig_princ,sig_rot,le_princ
-    stress_el(:, :,  1)  = Sstress;
-    stress_el(:, :,  2)  = Pstress;
-    stress_el(:, :,  3)  = sigma;
-    stress_el(:, :,  4)  = Estrain;
-    stress_el(:, :,  5)  = estrain;
-    stress_el(:, :,  6)  = eHstrain;
-    stress_el(:, :,  7)  = tau;
-    stress_el(:, :,  8)  = be;
-    stress_el(:, :,  9)  = dfdtau_tr;
-    stress_el(:, :, 10)  = dgdtau_tr;
-    stress_el(:, :, 11)  = sigma_princ;
-    stress_el(:, :, 12)  = sigma_rot;
-    stress_el(:, :, 13)  = eHstrain_princ;
+    stress_el(:, :,  1) = Sstress;
+    stress_el(:, :,  2) = Pstress;
+    stress_el(:, :,  3) = sigma;
+    stress_el(:, :,  4) = Estrain;
+    stress_el(:, :,  5) = estrain;
+    stress_el(:, :,  6) = eHstrain;
+    stress_el(:, :,  7) = tau;
+    stress_el(:, :,  8) = be;
+    stress_el(:, :,  9) = dfdtau_tr;
+    stress_el(:, :, 10) = dgdtau_tr;
+    stress_el(:, :, 11) = sigma_princ;
+    stress_el(:, :, 12) = sigma_rot;
+    stress_el(:, :, 13) = eHstrain_princ;
 
     % calculate stress invariants for output
-    sig_mean = trace(sigma)/3;
-    sig_dev  = sigma - sig_mean*eye_mat;
-    sig_VM   = sqrt(3/2)*norm(sig_dev);
+    sig_mean = trace(sigma) / 3;
+    sig_dev  = sigma - sig_mean * eye_mat;
+    sig_VM   = sqrt(3/2) * norm(sig_dev);
 
     % order of storage: p, q, c_tau, Dg, Jdef
     isv_el = [ sig_mean sig_VM c_tau Dg Jdef ];
 
     % return Fp
     Fp_el = Fp;
+end
+
+function C = coaxial(A, b)
+    [eig_vec, ~] = eig(A);
+    C            = zeros(3, 3);
+    for i=1:3
+        C = C + b(i) * eig_vec(:, i) * eig_vec(:, i)';
+    end
 end
