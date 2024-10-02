b8a04cb407fbe80d3a79530f67248a66a7f08880
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2024-10-02 09:47:11 -0600

    copy/paste enzyme-current

---
+++ nh-current/debugging/nh-common.h
@@ -0,0 +1,216 @@
+
+int SymmetricMatUnpack(const double sym[6], double full[3][3]) {
+  full[0][0] = sym[0];
+  full[0][1] = sym[5];
+  full[0][2] = sym[4];
+  full[1][0] = sym[5];
+  full[1][1] = sym[1];
+  full[1][2] = sym[3];
+  full[2][0] = sym[4];
+  full[2][1] = sym[3];
+  full[2][2] = sym[2];
+  return 0;
+}
+
+int SymmetricMatPack(const double full[3][3], double sym[6]) {
+  sym[0] = full[0][0];
+  sym[1] = full[1][1];
+  sym[2] = full[2][2];
+  sym[3] = full[1][2];
+  sym[4] = full[0][2];
+  sym[5] = full[0][1];
+  return 0;
+}
+
+int MatInverse(const double A[3][3], double det_A, double A_inv[3][3]) {
+  double B[3][3] = {
+      {A[1][1] * A[2][2] - A[1][2] * A[2][1], A[0][2] * A[2][1] - A[0][1] * A[2][2], A[0][1] * A[1][2] - A[0][2] * A[1][1]},
+      {A[1][2] * A[2][0] - A[1][0] * A[2][2], A[0][0] * A[2][2] - A[0][2] * A[2][0], A[0][2] * A[1][0] - A[0][0] * A[1][2]},
+      {A[1][0] * A[2][1] - A[1][1] * A[2][0], A[0][1] * A[2][0] - A[0][0] * A[2][1], A[0][0] * A[1][1] - A[0][1] * A[1][0]},
+  };
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      A_inv[i][j] = B[i][j] / (det_A);
+    }
+  }
+  return 0;
+}
+
+int MatMatMult(double alpha, const double A[3][3], const double B[3][3], double C[3][3]) {
+  for (int j = 0; j < 3; j++) {
+    for (int k = 0; k < 3; k++) {
+      C[j][k] = 0;
+      for (int m = 0; m < 3; m++) {
+        C[j][k] += alpha * A[j][m] * B[m][k];
+      }
+    }
+  }
+  return 0;
+}
+
+void VoigtUnpack(const double sym[6], double full[3][3]) {
+  full[0][0] = sym[0];
+  full[0][1] = sym[5];
+  full[0][2] = sym[4];
+  full[1][0] = sym[5];
+  full[1][1] = sym[1];
+  full[1][2] = sym[3];
+  full[2][0] = sym[4];
+  full[2][1] = sym[3];
+  full[2][2] = sym[2];
+};
+
+int MatComputeInverseSymmetric(const double A[3][3], const double det_A, double A_inv[6]) {
+  // Compute A^(-1) : A-Inverse
+  double B[6] = {
+      A[1][1] * A[2][2] - A[1][2] * A[2][1],
+      A[0][0] * A[2][2] - A[0][2] * A[2][0],
+      A[0][0] * A[1][1] - A[0][1] * A[1][0],
+      A[0][2] * A[1][0] - A[0][0] * A[1][2],
+      A[0][1] * A[1][2] - A[0][2] * A[1][1],
+      A[0][2] * A[2][1] - A[0][1] * A[2][2]
+  };
+  for (int m = 0; m < 6; m++) {
+    A_inv[m] = B[m] / (det_A);
+  }
+  return 0;
+};
+
+double MatDetAM1Symmetric(const double A_sym[6]) {
+  return A_sym[0] * (A_sym[1] * A_sym[2] - A_sym[3] * A_sym[3]) +          /* *NOPAD* */
+         A_sym[5] * (A_sym[3] * A_sym[4] - A_sym[5] * A_sym[2]) +          /* *NOPAD* */
+         A_sym[4] * (A_sym[5] * A_sym[3] - A_sym[4] * A_sym[1]) +          /* *NOPAD* */
+         A_sym[0] + A_sym[1] + A_sym[2] +                                  /* *NOPAD* */
+         A_sym[0] * A_sym[1] + A_sym[0] * A_sym[2] + A_sym[1] * A_sym[2] - /* *NOPAD* */
+         A_sym[5] * A_sym[5] - A_sym[4] * A_sym[4] - A_sym[3] * A_sym[3];  /* *NOPAD* */
+};
+
+double MatTraceSymmetric(const double A_sym[6]) { return A_sym[0] + A_sym[1] + A_sym[2]; }
+
+
+double MatDetAM1(const double A[3][3]) {
+    return A[0][0] * (A[1][1] * A[2][2] - A[1][2] * A[2][1]) +
+           A[0][1] * (A[1][2] * A[2][0] - A[1][0] * A[2][2]) +
+           A[0][2] * (A[1][0] * A[2][1] - A[2][0] * A[1][1]) +
+           A[0][0] + A[1][1] + A[2][2] +
+           A[0][0] * A[1][1] + A[0][0] * A[2][2] + A[1][1] * A[2][2] -
+           A[0][1] * A[1][0] - A[0][2] * A[2][0] - A[1][2] * A[2][1];
+};
+
+double VoigtDetAM1(const double V[6]) {
+    return V[0] * (V[1] * V[2] - V[3] * V[3]) +
+           V[5] * (V[3] * V[4] - V[5] * V[2]) +
+           V[4] * (V[5] * V[3] - V[4] * V[1]) +
+           V[0] + V[1] + V[2] +
+           V[0] * V[1] + V[0] * V[2] + V[1] * V[2] -
+           V[5] * V[5] - V[4] * V[4] - V[3] * V[3];
+};
+
+double Log1pSeries(double x) {
+    double sum = 0;
+    double y = x / (2. + x);
+    double y2 = y*y;
+    sum += y;
+    for (int i=0; i<5; i++) {
+      y *= y2;
+      sum += y / (2*i + 3);
+    }
+    return 2 * sum;
+};
+
+double VoigtTrace(double V[6]) { return V[0] + V[1] + V[2]; };
+
+void SecondPiolaKirchhoffStress_NeoHookean_Analytical(double E_Voigt[6], double S_Voigt[6], const double lambda, const double mu) {
+  double E2_Voigt[6];
+  for(int i = 0; i<6; i++) E2_Voigt[i] = 2*E_Voigt[i];
+
+  double E2[3][3];
+  VoigtUnpack(E2_Voigt, E2);
+
+  // Right Cauchy-Green tensor
+  double C[3][3] = {{1 + E2[0][0], E2[0][1], E2[0][2]},
+                     {E2[0][1], 1 + E2[1][1], E2[1][2]},
+                     {E2[0][2], E2[1][2], 1 + E2[2][2]}
+                    };
+
+  // Compute C^(-1) : C-Inverse
+  double Cinv_Voigt[6];
+  double C_inv[3][3];
+  double detCm1 = MatDetAM1(E2);
+  MatComputeInverseSymmetric(C, detCm1+1, Cinv_Voigt);
+  VoigtUnpack(Cinv_Voigt, C_inv);
+
+  // Compute the Second Piola-Kirchhoff (S)
+  int indj[6] = {0, 1, 2, 1, 0, 0}, indk[6] = {0, 1, 2, 2, 2, 1};
+  double logJ = Log1pSeries(detCm1) / 2.;
+  for ( int m = 0; m < 6; m++) {
+      S_Voigt[m] = lambda*logJ*Cinv_Voigt[m];
+      for ( int n = 0; n < 3; n++)
+          S_Voigt[m] += mu*C_inv[indj[m]][n]*E2[n][indk[m]];
+  }
+};
+
+int LinearStrain(const double grad_u[3][3], double e_sym[6]) {
+  e_sym[0] = grad_u[0][0];
+  e_sym[1] = grad_u[1][1];
+  e_sym[2] = grad_u[2][2];
+  e_sym[3] = (grad_u[1][2] + grad_u[2][1]) / 2.;
+  e_sym[4] = (grad_u[0][2] + grad_u[2][0]) / 2.;
+  e_sym[5] = (grad_u[0][1] + grad_u[1][0]) / 2.;
+  return 0;
+}
+
+int GreenEulerStrain(const double grad_u[3][3], double e_sym[6]) {
+  const int ind_j[6] = {0, 1, 2, 1, 0, 0}, ind_k[6] = {0, 1, 2, 2, 2, 1};
+
+  LinearStrain(grad_u, e_sym);
+  // Add (grad_u * grad_u^T)/2 term to the linear part of e_sym
+  for (int m = 0; m < 6; m++) {
+    for (int n = 0; n < 3; n++) {
+      e_sym[m] += (grad_u[ind_j[m]][n] * grad_u[ind_k[m]][n]) * 0.5;
+    }
+  }
+  return 0;
+}
+
+int GreenEulerStrain_fwd(const double grad_du[3][3], const double b[3][3], double de_sym[6]) {
+  const int ind_j[6] = {0, 1, 2, 1, 0, 0}, ind_k[6] = {0, 1, 2, 2, 2, 1};
+
+  for (int m = 0; m < 6; m++) {
+    de_sym[m] = 0;
+    for (int n = 0; n < 3; n++) {
+      de_sym[m] += (grad_du[ind_j[m]][n] * b[n][ind_k[m]] + b[ind_j[m]][n] * grad_du[ind_k[m]][n]) / 2.;
+    }
+  }
+  return 0;
+}
+
+double StrainEnergy(double E_Voigt[6], const double lambda, const double mu) {
+  // Calculate 2*E
+  double E2_Voigt[6];
+  for(int i = 0; i<6; i++) E2_Voigt[i] = E_Voigt[i] * 2;
+
+  // log(J)
+  double detCm1 = VoigtDetAM1(E2_Voigt);
+
+  //double J      = sqrt(detCm1 + 1);
+  double logJ   = Log1pSeries(detCm1) / 2.;
+
+  // trace(E)
+  double traceE = VoigtTrace(E_Voigt);
+
+  return lambda*logJ*logJ/2  + mu * (-logJ + traceE);
+};
+
+int MatMatTransposeMult(const double A[3][3], const double B[3][3], double C[3][3]) {
+  for (int j = 0; j < 3; j++) {
+    for (int k = 0; k < 3; k++) {
+      C[j][k] = 0;
+      for (int m = 0; m < 3; m++) {
+        C[j][k] += A[j][m] * B[k][m];
+      }
+    }
+  }
+  return 0;
+};
---
+++ nh-current/debugging/nh-enzyme.h
@@ -0,0 +1,66 @@
+#include <stdio.h>
+# include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "nh-common.h"
+
+double  StrainEnergy_NeoHookeanCurrentAD_Enzyme(double e_sym[6], double lambda, double mu) {
+  double e2_sym[6];
+
+  // J and log(J)
+  for (int i = 0; i < 6; i++) e2_sym[i] = 2 * e_sym[i];
+  const double detbm1 = MatDetAM1Symmetric(e2_sym);
+  const double J      = sqrt(detbm1 + 1);
+  const double logJ   = Log1pSeries(detbm1) / 2.;
+
+  // trace(e)
+  const double trace_e = MatTraceSymmetric(e_sym);
+
+  return lambda * (J * J - 1) / 4 - lambda * logJ / 2 + mu * (-logJ + trace_e);
+}
+
+/// @}
+
+// -----------------------------------------------------------------------------
+//  Compute Kirchhoff stress
+// -----------------------------------------------------------------------------
+// -- Enzyme-AD
+void       __enzyme_autodiff(void *, ...);
+void       __enzyme_fwddiff(void *, ...);
+extern int enzyme_const;
+
+void *__enzyme_function_like[2] = {(void *)Log1pSeries, (void *)"log1p"};
+
+/// @addtogroup Materials
+/// @{
+
+/**
+  @brief Compute Kirchoff tau for neo-Hookean hyperelasticity in current configuration with Enzyme
+
+  @param[in]   lambda    Lamé parameter
+  @param[in]   mu        Shear modulus
+  @param[in]   e_sym     Green Euler strain, in symmetric representation
+  @param[out]  tau_sym   Kirchoff tau, in symmetric representation
+**/
+ void Kirchhofftau_sym_NeoHookean_AD(const double lambda, const double mu, double e_sym[6],
+                                                               double tau_sym[6]) {
+  double dPsi_sym[6] = {0.}, b_sym[6], dPsi[3][3], b[3][3], tau[3][3];
+
+  // dPsi / de
+  __enzyme_autodiff((void *)StrainEnergy_NeoHookeanCurrentAD_Enzyme, e_sym, dPsi_sym, enzyme_const, lambda, enzyme_const, mu);
+  for (int i = 3; i < 6; i++) dPsi_sym[i] /= 2.;
+
+  // b = 2 e + I
+  for (int j = 0; j < 6; j++) b_sym[j] = 2 * e_sym[j] + (j < 3);
+
+  // tau = (dPsi / de) b
+  SymmetricMatUnpack(dPsi_sym, dPsi);
+  SymmetricMatUnpack(b_sym, b);
+  MatMatMult(1., dPsi, b, tau);
+  SymmetricMatPack(tau, tau_sym);
+}
+
+void dtau_fwd(const double lambda, const double mu, double e_sym[6], double de_sym[6],
+                                         double tau_sym[6], double dtau_sym[6]) {
+  __enzyme_fwddiff((void *)Kirchhofftau_sym_NeoHookean_AD, enzyme_const, lambda, enzyme_const, mu, e_sym, de_sym, tau_sym, dtau_sym);
+}
