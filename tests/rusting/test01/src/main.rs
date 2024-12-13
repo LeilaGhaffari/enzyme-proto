f01851bc83c6d4dee1380bf738b9ddd3acabe5c5
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2024-12-13 11:54:32 -0700

    rusting: update test01

--- tests/rusting/test01/src/main.rs
+++ tests/rusting/test01/src/main.rs
@@ -1,11 +1,20 @@
-// This program takes a list of numbers, computes their sum, and prints the result.
+// This program takes a list of numbers, computes their sum, and prints the average.
 
 fn main() {
-    let numbers = vec![1, 2, 3, 4, 5];
-    let sum = compute_sum(&numbers);
-    println!("The sum of {:?} is: {}", numbers, sum);
+    let mut numbers = vec![1, 2, 3, 4, 5];
+    let mult_const = 2;
+    let avg = compute_avg(&mut numbers, mult_const);
+    println!("The average of {:?} is: {}", numbers, avg/mult_const);
 }
 
-fn compute_sum(numbers: &[i32]) -> i32 {
+fn compute_sum(numbers: &mut [i32], mult_const: i32) -> i32 {
+    for n in numbers.iter_mut() {
+        *n *= mult_const;
+    }
     numbers.iter().sum()
+}
+
+fn compute_avg(numbers: &mut [i32], mult_const: i32) -> i32 {
+    let size = numbers.len() as i32;
+    compute_sum(numbers, mult_const) / size
 }
